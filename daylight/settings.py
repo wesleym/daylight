be2742dac5cdee44a8b2476fb4dae286ede29b51
Wesley Moy <wesley.moy@gmail.com>
2012-05-08 13:50:37 -0700

    Modify settings
    Set admin name and time zone.

--- daylight/settings.py
+++ daylight/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Wesley Moy', 'wesley.moy@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
